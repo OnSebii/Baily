410b7f7e0b17ffabece2d47247d213329b5ca29d
OnSebii <lang.sebastian.006@gmail.com>
2021-04-18 23:55:22 +0200

    Update Projects.vue

--- frontend/src/views/Projects.vue
+++ frontend/src/views/Projects.vue
@@ -1,5 +1,6 @@
 <template>
   <div class=""></div>
+  <!-- TODO: Auswahl zwischen Projekte und Github. Projekte: werden Projekte angezeigt; Github: mit der Github API hole ich mir alle Repos runter und zeige sie an -->
 </template>
 
 <script>
