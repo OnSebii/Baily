93285673abae7f36cb8de07729ab6a732c1c9a7d
OnSebii <lang.sebastian.006@gmail.com>
2021-05-06 10:29:42 +0200

    Update Home.vue

--- frontend-mdb/src/views/Home.vue
+++ frontend-mdb/src/views/Home.vue
@@ -2,12 +2,6 @@
   <div class="home">
     <div class="container-fluid p-0">
       <div class="row">
-        <div class="col">
-          <img src="https://codyhouse.co/demo/squeezebox-portfolio-template/img/img.png" alt="Girl in a jacket" />
-        </div>
-        <div class="col">
-          <h1 class="text-center">Hello, I'm Sebastian<br />Student, photographer and developer</h1>
-        </div>
       </div>
     </div>
   </div>
