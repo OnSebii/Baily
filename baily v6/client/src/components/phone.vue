14d8e40361dae07f09310351b7f67a1565a519fa
OnSebii <lang.sebastian.006@gmail.com>
2022-03-05 17:45:22 +0100

    v6 test

---
+++ baily v6/client/src/components/phone.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container>
+    <div class="iphone-x">
+      <i></i>
+      <b></b>
+      <s>10:24</s>
+    </div>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Phone',
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/css/iPhone.scss';
+</style>
---
+++ baily v6/client/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <phone></phone>
+    <icon></icon>
+  </div>
+</template>
+
+<script>
+import icon from '@/components/icon.vue';
+import phone from '@/components/phone.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    icon,
+    phone,
+  },
+};
+</script>
